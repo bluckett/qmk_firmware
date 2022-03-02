7b75ff208572197b8bc927b74fd8a41fa54f8555
bluckett <barryluckett@yahoo.com>
2022-03-02 11:33:04 -0500

    update keymap

--- keyboards/ungodly/launch_pad/keymaps/more/keymap.c
+++ keyboards/ungodly/launch_pad/keymaps/more/keymap.c
@@ -91,14 +91,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [0] = LAYOUT_ortho_5x4(
    KC_ESC, KC_PSLS,  KC_PAST,         KC_LUP,
-   KC_P7,    KC_P8,    KC_P9,        KC_PMNS,
-   KC_P4,    KC_P5,    KC_P6,        KC_PPLS,
-   KC_P1,    KC_P2,    KC_P3,        KC_PENT,
-   KC_P0,    KC_P0,  KC_PDOT,        KC_PENT
+   KC_7,    KC_8,    KC_9,        KC_PMNS,
+   KC_4,    KC_5,    KC_6,        KC_PPLS,
+   KC_1,    KC_2,    KC_3,        KC_PENT,
+   KC_0,    KC_0,  KC_DOT,        KC_PENT
  ),
 
 /* Keymap _NV: Navigation layer
-* ,-------------------.
+* ,-------------------.qm
 * |LAYR|    |    |NUM |
 * |----|----|----|----|
 * |HOME| UP |PGUP|    |
@@ -350,7 +350,7 @@ static void render_light_logo(void) {
   oled_write_raw_P(light_logo, sizeof(light_logo));
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   switch (get_highest_layer(layer_state)) {
     case 0:
       render_rocket_logo();
@@ -372,5 +372,6 @@ void oled_task_user(void) {
       oled_write_ln_P(PSTR(" UND"), false);
       break;
     }
+	return false;
 }
 #endif
